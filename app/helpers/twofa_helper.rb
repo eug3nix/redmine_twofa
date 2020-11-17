9e8953f7eb9bbe1a9b296a08ebb9716b86338c4b
Eugene Dubinin <eugend@commandprompt.com>
2020-11-17 14:55:21 +0200

    adds check for my_account form method, post vs put

--- app/helpers/twofa_helper.rb
+++ app/helpers/twofa_helper.rb
@@ -2,4 +2,13 @@ module TwofaHelper
   def require_active_twofa
     Setting.twofa? ? true : deny_access
   end
+
+  def my_account_form_method
+      begin
+        Rails.application.routes.recognize_path('/my/account', method: 'PUT')
+        return 'put'
+      rescue ActionController::RoutingError
+        return 'post'
+      end
+  end
 end
